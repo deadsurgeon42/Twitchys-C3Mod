2ea373093ff698b73d59bb3afc5838bfe1c90060
Ruby Rose <newy@aurora-terraria.org>
2016-11-20 23:32:56 +0300

    Clean code

--- C3Player.cs
+++ C3Player.cs
@@ -1,49 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using Terraria;
 using TShockAPI;
 
 namespace C3Mod
 {
-    public class C3Player
+  public class C3Player
+  {
+    public int ChallengeNotifyCount = 0;
+    public DateTime ChallengeTick = DateTime.UtcNow;
+    public C3Player Challenging;
+    public bool Dead = false;
+    public int FFAScore = 0;
+    public string GameType = "";
+    public int Index;
+    public C3Player KillingPlayer = null;
+    public DateTime LastSpawn = DateTime.UtcNow;
+    public int LivesUsed = 0;
+    public bool SpawnProtectionEnabled = false;
+    public bool Spectator = false;
+    public int Team = 0;
+    public Vector2[] tempflags = new Vector2[2];
+    public Vector2[] tempspawns = new Vector2[2];
+
+    public C3Player(int index)
     {
-        public int Index;
-        public int Team = 0;
-        public bool TerrariaDead { get { return Main.player[Index].dead; } }
-        public bool Dead = false;
-        public string GameType = "";
-        public int TerrariaTeam { get { return Main.player[Index].team; } }
-        public string PlayerName { get { return Main.player[Index].name; } }
-        public float tileX { get { return (Main.player[Index].position.X / 16); } }
-        public float tileY { get { return (Main.player[Index].position.Y / 16); } }
-        public TSPlayer TSPlayer { get { return (C3Tools.GetTSPlayerByIndex(Index)); } }
-        public C3Player Challenging;
-        public int ChallengeArena { get; set; }
-        public DateTime ChallengeTick = DateTime.UtcNow;
-        public int ChallengeNotifyCount = 0;
-        public bool Spectator = false;
-        public int LivesUsed = 0;
-        public Vector2[] tempflags = new Vector2[2];
-        public Vector2[] tempspawns = new Vector2[2];
-        public int FFAScore = 0;
-        public DateTime LastSpawn = DateTime.UtcNow;
-        public bool SpawnProtectionEnabled = false;
-        public C3Player KillingPlayer = null;
+      Index = index;
+    }
 
-        public C3Player(int index)
-        {
-            Index = index;
-        }
+    public bool TerrariaDead { get { return Main.player[Index].dead; } }
+    public int TerrariaTeam { get { return Main.player[Index].team; } }
+    public string PlayerName { get { return Main.player[Index].name; } }
+    public float tileX { get { return (Main.player[Index].position.X / 16); } }
+    public float tileY { get { return (Main.player[Index].position.Y / 16); } }
+    public TSPlayer TSPlayer { get { return (C3Tools.GetTSPlayerByIndex(Index)); } }
+    public int ChallengeArena { get; set; }
 
-        public void SendMessage(string message, Color color)
-        {
-            NetMessage.SendData((int)PacketTypes.ChatText, Index, -1, message, 255, color.R, color.G, color.B);
-        }
+    public void SendMessage(string message, Color color)
+    {
+      NetMessage.SendData((int)PacketTypes.ChatText, Index, -1, message, 255, color.R, color.G, color.B);
+    }
 
-        public void GiveItem(int type, string name, int width, int height, int stack)
-        {
-            TShock.Players[Index].GiveItem(type, name, width, height, stack);
-        }
-    }    
+    public void GiveItem(int type, string name, int width, int height, int stack)
+    {
+      TShock.Players[Index].GiveItem(type, name, width, height, stack);
+    }
+  }
 }
